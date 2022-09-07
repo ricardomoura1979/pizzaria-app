ff8e44e0ec71cac63cac5e97349e7d762ccd389b
ricardomoura1979 <73795249+ricardomoura1979@users.noreply.github.com>
2022-09-07 10:20:48 -0500

    added user login

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 
 import { CreateUserController } from './controllers/user/CreateUserController'
+import { AuthUserController } from './controllers/AuthUserController'; 
 
 const router = Router();
 
 //-- ROTAS USER --
 router.post('/users', new CreateUserController().handle)
-
+router.post('/session', new AuthUserController().handle)
 export { router }; 
