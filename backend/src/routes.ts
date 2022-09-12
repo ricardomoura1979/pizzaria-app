e7ab7056fb5da0c15252dd5ebd5a839c0506fdc6
ricardomoura1979 <73795249+ricardomoura1979@users.noreply.github.com>
2022-09-12 07:25:44 -0400

    added middleware to return user details from database

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,18 +1,18 @@
 import { Router } from 'express';
 
 import { CreateUserController } from './controllers/user/CreateUserController'
-import { AuthUserController } from './controllers/user/AuthUserController'; 
-import { DetailuserController, DetailUserController } from './controllers/user/DetailUserController'
+import { AuthUserController } from './controllers/user/AuthUserController'
+import { DetailuserController } from './controllers/user/DetailUserController'
+
+import { isAuthenticated } from './middlewares/isAuthenticated'
 
 const router = Router();
 
 //-- ROTAS USER --
 router.post('/users', new CreateUserController().handle)
+
 router.post('/session', new AuthUserController().handle)
-router.get('/me', new DetailuserController().handle)
 
-
-
-
+router.get('/me', isAuthenticated,  new DetailuserController().handle )
 
 export { router }; 
