7fd228b78a951b3cd8d2228769b5a06dc373b4ab
ricardomoura1979 <73795249+ricardomoura1979@users.noreply.github.com>
2022-09-20 15:43:51 -0400

    added category products listing

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -9,7 +9,7 @@ import { CreateCategoryController } from './controllers/category/CreateCategoryC
 import { ListCategoryController } from './controllers/category/ListCategoryController';
 
 import { CreateProductController } from './controllers/product/CreateProductController';
-
+import { ListByCategoryController } from './controllers/product/ListBycategoryController';
 
 import { isAuthenticated } from './middlewares/isAuthenticated'
 import uploadConfig from './config/multer'
@@ -37,5 +37,7 @@ router.get('/category', isAuthenticated, new ListCategoryController().handle)
 // Rotas de Produtos
 
 router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle)
+
+router.get('/category/product', isAuthenticated, new ListByCategoryController().handle)
 
 export { router };
