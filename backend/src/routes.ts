a438c3ec5a286afb8fba87a928f348165c18b519
ricardomoura1979 <73795249+ricardomoura1979@users.noreply.github.com>
2022-09-21 14:57:52 -0400

    added order creation feature

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -11,6 +11,8 @@ import { ListCategoryController } from './controllers/category/ListCategoryContr
 import { CreateProductController } from './controllers/product/CreateProductController';
 import { ListByCategoryController } from './controllers/product/ListBycategoryController';
 
+import { CreateOrderController } from './controllers/order/CreateOrderController';
+
 import { isAuthenticated } from './middlewares/isAuthenticated'
 import uploadConfig from './config/multer'
 
@@ -39,5 +41,9 @@ router.get('/category', isAuthenticated, new ListCategoryController().handle)
 router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle)
 
 router.get('/category/product', isAuthenticated, new ListByCategoryController().handle)
+
+// ROTAS ORDER
+
+router.post('/order', isAuthenticated, new CreateOrderController().handle)
 
 export { router };
